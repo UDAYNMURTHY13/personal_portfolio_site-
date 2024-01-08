e54785c10c84266a4076f79e59200cb2498a02a7
UDAYNMURTHY13 <udaynarasimhamurthy@gmail.com>
2024-01-08 15:01:42 +0530

    add files

---
+++ U.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void int()
+{
+    int i,n,sum;
+    printf("ENTER THE VALUE OF N");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        sum=sum+i;
+    }
+    return 0;
+}
